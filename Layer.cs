1774cf0fa9bb6a5c3131c7ce9005b2db7cb01f9a
philcapon <phil.capon@fastmail.co.uk>
2020-04-18 23:06:20 +0100

    early class defs

---
+++ Layer.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace core_image_recog
+{
+    class Layer
+    {
+    }
+}
---
+++ Neuron.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace core_image_recog
+{
+    class Neuron
+    {
+        public double Bias { get; set; }
+        public double[] Weights { get; set; }
+
+        public double Output(double[] x)
+        {
+            return 0d;
+        }
+    }
+}
