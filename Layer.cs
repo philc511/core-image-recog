23e5dbd83939d7b43c47117c31a387e7bec779b1
philc511 <phil.capon@fastmail.co.uk>
2020-04-19 18:17:06 +0100

    update layer

--- Layer.cs
+++ Layer.cs
@@ -1,8 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 namespace core_image_recog
 {
-    class Layer
+    public class Layer
     {
+        private int size;
+
+        private Neuron[] neurons;
+
+        public Layer(int size)
+        {
+            this.size = size;
+
+            this.neurons = new Neuron[size];
+            for (int i = 0; i < size; i++)
+            {
+                this.neurons[i] = new Neuron();
+            }
+        }
+        public double[] Output(double[] x)
+        {
+            var output = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                output[i] = this.neurons[i].Output(x);
+            }
+            return output;
+        }
+
     }
 }
